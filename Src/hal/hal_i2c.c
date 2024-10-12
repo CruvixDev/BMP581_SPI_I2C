90558d64198e4fed6c33d9def4f9dedac625fd98
CruvixDev <100412344+CruvixDev@users.noreply.github.com>
2024-10-12 14:34:17 +0200

    Left shift address for HAL

--- Src/hal/hal_i2c.c
+++ Src/hal/hal_i2c.c
@@ -186,7 +186,7 @@ void vI2C_read(sI2CSensor_t* p_pi2cSensorInfo, uint8_t p_u8ReadAddress, uint8_t*
   if (p_pi2cSensorInfo != NULL) {
     HAL_I2C_Mem_Read_DMA(
       &hi2c1,
-      (uint16_t)p_pi2cSensorInfo->u8_i2cAddress, //Left shift of one byte for the HAL needed
+      (uint16_t)p_pi2cSensorInfo->u8_i2cAddress << 1, //Left shift of one byte for the HAL needed
       (uint16_t)p_u8ReadAddress,
       (uint16_t)p_pi2cSensorInfo->u8_i2cRegisterSize,
       p_pu8Data,
