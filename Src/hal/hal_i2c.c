03afafc2894a848fc27dcb55b60e7318eb89158d
CruvixDev <100412344+CruvixDev@users.noreply.github.com>
2024-10-15 22:15:00 +0200

    Use DMA to read address and move all memory into D1 domain

--- Src/hal/hal_i2c.c
+++ Src/hal/hal_i2c.c
@@ -184,7 +184,7 @@ void vI2C_write(sI2CSensor_t* p_pi2cSensorInfo, uint8_t p_u8WriteAddress, uint8_
 
 void vI2C_read(sI2CSensor_t* p_pi2cSensorInfo, uint8_t p_u8ReadAddress, uint8_t* p_pu8Data, uint16_t p_u16Size) {
   if (p_pi2cSensorInfo != NULL) {
-    HAL_I2C_Mem_Read_IT(
+    HAL_I2C_Mem_Read_DMA(
       &hi2c1,
       (uint16_t)p_pi2cSensorInfo->u8_i2cAddress << 1, //Left shift of one byte for the HAL needed
       (uint16_t)p_u8ReadAddress,
